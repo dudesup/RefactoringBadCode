8c75f2a9e8a95685d414770be5c4ae0002e99dc0
Daria Supińska <dariasupinska@gmail.com>
2022-11-19 23:29:34 +0100

    shouldParseEmptyFolder test added

--- src/test/java/com/cd/bad/code/XMLToJsonTest.java
+++ src/test/java/com/cd/bad/code/XMLToJsonTest.java
@@ -1,9 +1,15 @@
 package com.cd.bad.code;
 
 import org.approvaltests.Approvals;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
 
 public class XMLToJsonTest {
     @Test
@@ -14,5 +20,18 @@ public class XMLToJsonTest {
         String xPathString = "fk:AMM24_fk:AMM24-00-00_fk:AMM24-00-00-02";
 
         Approvals.verify(translate.getJson(url, xPathString));
+    }
+
+    @Test
+    public void shouldParseEmptyFolder() throws Exception {
+        final XMLToJson translate = new XMLToJson();
+        final Document document = documentFor("<folder></folder>");
+        assertEquals("[]", translate.getJsonFromDocument("/", document));
+    }
+
+
+    private Document documentFor(String xml) throws DocumentException {
+        var reader = new SAXReader();
+        return reader.read(new ByteArrayInputStream(xml.getBytes()));
     }
 }
