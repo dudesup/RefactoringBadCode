fd424445d89b6bc9d6f30389e5b1e6b9695b2c0f
Daria Supińska <dariasupinska@gmail.com>
2022-11-19 23:23:08 +0100

    XMLToJson adjusted

--- src/main/java/com/cd/bad/code/XMLToJson.java
+++ src/main/java/com/cd/bad/code/XMLToJson.java
@@ -1,11 +1,14 @@
 package com.cd.bad.code;
 
-import org.dom4j.Attribute;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 import java.net.URL;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * fk--folder key,dk--doc key -->value is key string
@@ -38,219 +41,69 @@ import java.util.*;
  */
 
 public class XMLToJson {
-    private static final Map<String, String> pathMap;
+    private static final String EMPTY_STRING = "";
+    private static final Map<String, TocElement> ELEMENTS;
 
     static {
-        Map<String, String> aMap = new HashMap<String, String>();
-        aMap.put("fk", "folder[@key");
-        aMap.put("ft", "folder[@type");
-        aMap.put("fth", "folder[@type='history'");
-        aMap.put("dk", "doc[@key");
-        aMap.put("dt", "doc[@type");
-        aMap.put("dth", "doc[@type='history'");
-        aMap.put("dtrn", "doc[@trnum");
-        pathMap = Collections.unmodifiableMap(aMap);
+        ELEMENTS = new HashMap<>();
+        ELEMENTS.put("folder", new FolderElement());
+        ELEMENTS.put("doc", new DocElement());
     }
 
-    Util util = new Util();
+    private XPathProvider xPathProvider;
 
-
-    @SuppressWarnings({"unchecked"})
-    public String getJson(URL urlToTOC, String xPathString) throws Exception {
-        Document TOCDoc = util.getDocument(urlToTOC);
-        return getJsonForDoc(xPathString, TOCDoc);
-
+    public XMLToJson() {
+        this(new TocQueryExpressions());
     }
 
-    public String getJsonForDoc(String xPathString, Document TOCDoc) throws Exception {
+    public XMLToJson(XPathProvider xPathProvider) {
+        this.xPathProvider = xPathProvider;
+    }
+
+    public String getJson(URL url, String xPathString) throws Exception {
+        Document TOCDoc = getDocument(url);
+        return getJsonFromDocument(xPathString, TOCDoc);
+    }
+
+    public String getJsonFromDocument(String xPathString, Document TOCDoc) throws Exception {
         String jsonString = "[";
 
         Element node = getNode(xPathString, TOCDoc);
 
         for (Iterator<Element> i = node.elementIterator(); i.hasNext(); ) {
-            jsonString = processElement(xPathString, jsonString, i);
+            jsonString = jsonString.concat(getProcessedElement(xPathString, i.next()));
         }
 
-        return closeJson(jsonString);
+        return getClosedJson(jsonString);
     }
 
-    private String closeJson(String jsonString) {
-        jsonString = jsonString.substring(0, jsonString.length() - 1);
+    private Document getDocument(URL url) throws DocumentException {
+        SAXReader reader = new SAXReader();
+        return reader.read(url);
+    }
+
+    private String getClosedJson(String jsonString) {
+        if (jsonString.length() > 1) {
+            jsonString = jsonString.substring(0, jsonString.length() - 1);
+        }
         jsonString = jsonString.concat("]");
         return jsonString;
     }
 
-    public static Map<String, TocElement> elements;
-    public static final DocElement DOC_ELEMENT = new DocElement();
-    public static final FolderElement FOLDER_ELEMENT = new FolderElement();
-
-    static {
-        elements = new HashMap<String, TocElement>();
-        elements.put("doc", DOC_ELEMENT);
-        elements.put("folder", FOLDER_ELEMENT);
-    }
-
-    private String processElement(String xPathString, String jsonString, Iterator<Element> i) {
-        Element elem = (Element) i.next();
-        String eleName = elem.getName();
-
-        List<Attribute> list = elem.attributes();
-        String titleAttrContent = elem.attributeValue("title");
-
-        String fileAttrContent = elem.attributeValue("file");
-        //if  (fileAttrContent.isEmpty() )
-        if (eleName == "doc") {
-
-            for (Attribute attribute : list) {
-                jsonString = jsonString.concat("{");
-                String attrName = attribute.getName();
-
-                jsonString = jsonString.concat("'data':'").concat(titleAttrContent).concat("',");
-                if (attrName.equals("key")) {
-                    String keyContent = elem.attributeValue("key");
-                    jsonString = jsonString.concat("'attr':{'id':'").concat(xPathString).concat("_dk:").concat(keyContent).concat("','file':'").concat(fileAttrContent).concat("'}");
-
-                    break;
-                } else if (attrName.equals("trnum")) {
-
-                    String trnumContent = elem.attributeValue("trnum");
-                    jsonString = jsonString.concat("'attr':{'id':'").concat(xPathString).concat("_dtrn:").concat(trnumContent).concat("','file':'").concat(fileAttrContent).concat("'}");
-
-                    break;
-                }
-
-            }
-            if (hasChildren(elem)) {
-                jsonString = jsonString.concat(",'state':'closed'");
-            }
-            jsonString = jsonString.concat("},");
-        } else if (eleName == "folder") {
-            jsonString = jsonString.concat("{");
-            for (Attribute attribute : list) {
-                String attrName = attribute.getName();
-                jsonString = jsonString.concat("'data':'").concat(titleAttrContent).concat("',");
-                if (attrName.equals("key")) {
-                    String keyContent = elem.attributeValue("key");
-                    jsonString = jsonString.concat("'attr':{'id':'").concat(xPathString).concat("_fk:").concat(keyContent).concat("'}");
-                    if (fileAttrContent != null) {
-                        jsonString = jsonString.concat("','file':'").concat(fileAttrContent).concat("'}");
-                    }
-
-                    break;
-                } else if (attrName.equals("type")) {
-                    String typeContent = elem.attributeValue("type");
-                    //doc element has type "history"
-                    if (typeContent == "history") {
-                        jsonString = jsonString.concat("'attr':{'id':'").concat(xPathString).concat("_fth,");
-
-                    }
-                    break;
-                }
-            }
-            jsonString = jsonString.concat("},");
+    private String getProcessedElement(String xPathExpression, Element element) {
+        String elementName = element.getName();
+        if (ELEMENTS.containsKey(elementName)) {
+            return ELEMENTS.get(elementName).processElement(xPathExpression, element);
         }
-        return jsonString;
+        return EMPTY_STRING;
     }
 
     private Element getNode(String xPathString, Document TOCDoc) throws Exception {
-        Element node = null;
         if (xPathString.equals("/")) {
-
-            node = TOCDoc.getRootElement();
+            return TOCDoc.getRootElement();
         } else {
-            String realXPathString = pathMapping(xPathString);
-            System.out.println(realXPathString);
-            node = (Element) TOCDoc.selectSingleNode(realXPathString);
+            String realXPathString = this.xPathProvider.xPathFrom(xPathString);
+            return (Element) TOCDoc.selectSingleNode(realXPathString);
         }
-        return node;
-    }
-
-    private boolean hasChildren(Element elem) {
-        return elem.elements().size() > 0;
-    }
-
-    /*
-     * read xpathstring from post request and generate the real xpath for toc
-     */
-    public String getXPathString() {
-        return null;
-    }
-
-    /*
-     * post string looks like : "fk:LOETR_dtrn:TR12-118_fth_dth"
-     * it represents the inner doc elemnet:
-     * <folder key="LOETR" type="loetr" title="List of Effective TRs" file="loetr.html">
-     *		<doc type="tr" trnum="TR12-118" trdate="May 07/2012" title="[TR12-118] TASK AMM12-31-00-660-806 - Inspection and Removal of De-Hydrated Anti-Icing Fluid inside the Flight Control Surfaces" file="TR12-118.pdf" refloc="AMM12-31-00-660-806">
-     * 			<folder type="history" title="History of AMM12-31-00-660-806">
-     *   			<doc title="TASK 12-31-00-660-806 - Inspection and Removal of De-Hydrated Anti-Icing Fluid inside the Flight Control Surfaces" file="AMM12-31-00-660-806.pdf" type="history" refloc="AMM12-31-00-660-806"/>
-     * </folder>
-     * the xpath string should be:
-     * folder[@key="LOETR"]/doc[@trnum="TR12-118"]/folder[@type="history"]/doc[@type="history"]
-     *
-     *
-     * the String : "fk:AMM24_fk:AMM24-FM_dk:CTOC-24"
-     * it represents the inner doc with attribute file="CTOC-24.pdf"
-     * the string : "fk:AMM24_fk:AMM24-00-00_fk:AMM24-00-00-02_dk:AMM24-00-00-700-801" represents
-     * <folder key="AMM24" title="CH 24 - Electrical Power">
-     *		<folder key="AMM24-FM" title="Front Matter">
-     * 			<doc key="CTOC-24" title="Table of Contents" file="CTOC-24.pdf"/>
-     *		</folder>
-     *		<folder key="AMM24-00-00" title="24-00-00 - General">
-     * 			<folder key="AMM24-00-00-02" title="General - Maintenance Practices">
-     *   			<doc key="AMM24-00-00-700-801" title="TASK 24-00-00-700-801 - AC Power, DC Power and Battery Maintenance Practice Recommendations" file="AMM24-00-00-700-801.pdf"/>
-     *
-     * it can be even optimized as :
-     * "fk:AMM24_fk:00-00_fk:02_dk:AMM24-00-00-700-801"
-     * if the inner key fully include the previous key, omit it, otherwise use full string
-     * the xpath string should be:
-     * folder[@key="AMM24"]/folder[@key="AMM24-00-00"]/folder[@key="AMM24-00-00-02"]/doc[@key="AMM24-00-00-700-801"]
-     *
-     * if shortXPath is ?? which means the query based on the root of the document
-     *
-     *
-     */
-    public String pathMapping(String shortXPath) throws Exception {
-        String tagetString = null;
-        if (shortXPath.equals("")) {
-            tagetString = "//toc";
-        } else {
-            tagetString = "//";
-        }
-
-        int newStart = 0;
-        String segString = "";
-        String valueString = "";
-        while (shortXPath.indexOf("_", newStart) > -1) {
-            int keyValueSepPos = 0;
-            String keyString = "";
-            segString = shortXPath.substring(newStart, shortXPath.indexOf("_", newStart));
-            newStart = shortXPath.indexOf("_", newStart) + 1;
-            if (segString.indexOf(":") > 0) {
-                keyValueSepPos = segString.indexOf(":");
-                keyString = segString.substring(0, keyValueSepPos);
-                valueString = segString.substring(keyValueSepPos + 1);
-                if (pathMap.get(keyString).length() > 0) {
-                    tagetString = tagetString.concat(pathMap.get(keyString));
-                } else {
-                    throw new Exception("no mapping found");
-                }
-                tagetString = tagetString.concat("='").concat(valueString).concat("']/");
-            }
-        }
-        segString = shortXPath.substring(newStart);
-        System.out.println(segString);
-        if (segString.indexOf(":") > 0) {
-            int lastKeyValueSepPos = segString.indexOf(":");
-            String lastKeyString = segString.substring(0, lastKeyValueSepPos);
-            String lastValueString = segString.substring(lastKeyValueSepPos + 1);
-            if (pathMap.get(lastKeyString).length() > 0) {
-                tagetString = tagetString.concat(pathMap.get(lastKeyString));
-            } else {
-                throw new Exception("no mapping found");
-            }
-            tagetString = tagetString.concat("='").concat(lastValueString).concat("']");
-        }
-        return tagetString;
-
     }
 }
